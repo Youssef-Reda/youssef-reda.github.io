89661768c469d0772156c16344e38a8a8120a8c3
Youssef  Reda <yousefreda_gad99@live.com>
2021-08-01 01:28:01 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,41 +1,16 @@
 <?php
+  if(isset($_POST['sendEmail'])) {
+    $privateKey = "6LddIdEbAAAAAOmVWrvAMAAnCVJxB11EHI1-XZ5H";
+    $response = $_POST['g-recaptcha-response'];
+    $remoteip = $_SERVER['REMOTE_ADDR'];
+    $url = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$privateKey&response=$response&remoteip=$remoteip");
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $result = json_decode($url, true);
 
-        // access
-        $secretKey = '6LddIdEbAAAAAOmVWrvAMAAnCVJxB11EHI1-XZ5H';
-        $captcha = $_POST['g-recaptcha-response'];
-
-        if(!$captcha){
-          echo '<p class="alert alert-warning">Please check the the captcha form.</p>';
-          exit;
-        }
-
-        # FIX: Replace this email with recipient email
-        $mail_to = "yousefreda_gad99@live.com";
-
-        $ip = $_SERVER['REMOTE_ADDR'];
-        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
-        $responseKeys = json_decode($response,true);
-
-        if(intval($responseKeys["success"]) !== 1) {
-          echo '<p class="alert alert-warning">Please check the the captcha form.</p>';
-        } else {
-            # Send the email.
-            $success = mail($mail_to);
-            if ($success) {
-                # Set a 200 (okay) response code.
-                http_response_code(200);
-            } else {
-                # Set a 500 (internal server error) response code.
-                http_response_code(500);
-                echo '<p class="alert alert-warning">Oops! Something went wrong, I could not recieve your message.</p>';
-            }
-        }
+    if($result->success == true) {
 
     } else {
-        # Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
-        echo '<p class="alert alert-warning">There was a problem with your submission, please try again.</p>';
+      echo "reCaptcha failed, please try again...";
     }
+  }
 ?>
